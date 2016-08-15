8d904c4f4566d244fa4d3c3300ae07d9cfd398aa
Jacques Uys <jaxuys@gmail.com>
2016-08-15 11:56:20 +0200

    drop downs store user selection, carets are css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
   $('#datetimepicker1').datetimepicker();
   $('#datetimepicker2').datetimepicker();
+
+  $('.dropdown a').on('click', function(e){
+    e.preventDefault();
+    var text = $(this).text();
+    var $root = $(this).parent().parent().parent();
+    
+    $root.find('.btn').text(text);
+    $root.parent().find('input[type="hidden"]').val(text);
+  });
 });

689efe82e06ebb7273c1a851431b67c343057356
Jacques Uys <jaxuys@gmail.com>
2016-08-17 19:27:55 +0200

    regex object

--- js/main.js
+++ js/main.js
@@ -11,34 +11,34 @@ $(document).ready(function() {
     $root.parent().find('input[type="hidden"]').val(text);
   });
 
-  $('form').on('submit', function(e) {
-    // '/([0-9, A-Z])\w+/'
-
-    // The regexes
-    var title = /(Mr?s?)/g;
-    var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var word = /\w+/gi; // this needs more work
-    var tel = /\b\d{3}[-.\s]?\d{3}[-.\s]]?\d{4}\b/;
-    var number = /[0-9]/g;
-
-    var regex = {
-      isText: function(str) {
-        return word.test(str);
-      },
-      isEmail: function(email) {
-        return email.test(email);
-      },
-      isTel: function(tel) {
-        return tel.test(tel);
-      },
-      isTitle: function(title) {
-        return title.test(title);
-      },
-      isNumber: function(num) {
-        return number.test(num);
-      }
+  var regex = {
+    title: /(Mr?s?)/,
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    word: /[a-z]/i,
+    tel: /\b\d{3}[-.\s]?\d{3}[-.\s]]?\d{4}\b/,
+    date: /\d{2}[-\/]\d{2}[-\/]\d{4}[\s]\d{2}:\d{2}[\s][a|p]m/,
+    number: /[0-9]/g,
+    isText: function(val) {
+      return this.word.test(val);
+    },
+    isEmail: function(val) {
+      return this.email.test(val);
+    },
+    isTel: function(val) {
+      return this.tel.test(val);
+    },
+    isDate: function(val) {
+      return this.date.test(val);
+    },
+    isTitle: function(val) {
+      return this.title.test(val);
+    },
+    isNumber: function(val) {
+      return this.number.test(val);
     }
+  };
 
+  $('form').on('submit', function(e) {
     // * = non hidden inputs
 
     // title
@@ -62,9 +62,6 @@ $(document).ready(function() {
     // baby *
     // agent
     // newsletter *
-
-
-
 
     e.preventDefault();
     console.log($(this).serialize());

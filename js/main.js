633f43e2337a2e2732dd8a8eb1af1b1cb0308d5a
Jacques Uys <jaxuys@gmail.com>
2016-08-17 13:14:36 +0200

    basic regex for the form

--- js/main.js
+++ js/main.js
@@ -13,6 +13,32 @@ $(document).ready(function() {
 
   $('form').on('submit', function(e) {
     // '/([0-9, A-Z])\w+/'
+
+    // The regexes
+    var title = /(Mr?s?)/g;
+    var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var word = /\w+/gi; // this needs more work
+    var tel = /\b\d{3}[-.\s]?\d{3}[-.\s]]?\d{4}\b/;
+    var number = /[0-9]/g;
+
+    var regex = {
+      isText: function(str) {
+        return word.test(str);
+      },
+      isEmail: function(email) {
+        return email.test(email);
+      },
+      isTel: function(tel) {
+        return tel.test(tel);
+      },
+      isTitle: function(title) {
+        return title.test(title);
+      },
+      isNumber: function(num) {
+        return number.test(num);
+      }
+    }
+
     e.preventDefault();
     console.log($(this).serialize());
     // $('#myModal').modal('show');
